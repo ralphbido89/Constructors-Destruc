46935f73554a3a3bb18d41599ef145fc4a996c26
Ralph Bido <rbido@wgu.edu>
2023-10-01 14:38:01 -0700

    Const Initialization List

--- main.cpp
+++ main.cpp
@@ -23,7 +23,19 @@ class Hero {
         std::string get_name() {
             return name;
         }
+        Hero::Hero(std::string name_val, int health_val, double xp_val) {
+            name = name_val;
+            strength = health_val;
+            agility = xp_val;
 
+        }
+        
+        // CONSTRUCTOR INITIALIZATION LIST
+        Hero::Hero() : name{"None"}, strength{0}, agility{0} {
+
+        }
+        Hero::Hero(std::string name_val) : name{name_val} {}
+        Hero::Hero(int agility_val) : agility{agility_val} {}
 
 };
 
@@ -37,6 +49,8 @@ int main() {
     Hero tidehunter {"Tidehunter", 23, 1.4, 33};
     tidehunter.set_name("Tidehunter");
     std::cout << tidehunter.get_name() << std::endl;
+    Hero phantom {"Phantom", 23, 3.4};
+    phantom.set_name("Phantom");
 
 
     return 0;
